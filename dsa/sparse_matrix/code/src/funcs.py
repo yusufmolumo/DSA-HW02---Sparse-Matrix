ec8327e7c831b98d77582f6415cdcad9d7c71722
yusufmolumo <144047887+yusufmolumo@users.noreply.github.com>
2024-05-31 13:33:15 -0700

    Create funcs.py

---
+++ dsa/sparse_matrix/code/src/funcs.py
@@ -0,0 +1,71 @@
+MIN_RUN = 32
+
+def insertion_sort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= left and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def merge(arr, left, mid, right):
+    len1, len2 = mid - left + 1, right - mid
+    left_part = arr[left:mid + 1]
+    right_part = arr[mid + 1:right + 1]
+
+    i, j, k = 0, 0, left
+
+    while i < len1 and j < len2:
+        if left_part[i] <= right_part[j]:
+            arr[k] = left_part[i]
+            i += 1
+        else:
+            arr[k] = right_part[j]
+            j += 1
+        k += 1
+
+    while i < len1:
+        arr[k] = left_part[i]
+        i += 1
+        k += 1
+
+    while j < len2:
+        arr[k] = right_part[j]
+        j += 1
+        k += 1
+
+def SortIntLines(arr):
+    n = len(arr)
+    for i in range(0, n, MIN_RUN):
+        insertion_sort(arr, i, min(i + MIN_RUN - 1, n - 1))
+
+    size = MIN_RUN
+    while size < n:
+        for left in range(0, n, 2 * size):
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), (n - 1))
+
+            if mid < right:
+                merge(arr, left, mid, right)
+
+        size *= 2
+
+
+def strip_string(s, chars=None):
+    if chars is None:
+        chars = ' \t\n\r\v\f'
+
+    start = 0
+    end = len(s) - 1
+
+    # Remove leading characters
+    while start <= end and s[start] in chars:
+        start += 1
+
+    # Remove trailing characters
+    while end >= start and s[end] in chars:
+        end -= 1
+
+    # Return the sliced string
+    return s[start:end+1]
